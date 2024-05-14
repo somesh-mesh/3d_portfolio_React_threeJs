04d32a9a372e62f8c028c8db2edc5782656d9abc
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-05-14 16:21:03 +0530

    3d portfolio

--- 3d_portfolio/src/pages/Home.jsx
+++ 3d_portfolio/src/pages/Home.jsx
@@ -40,9 +40,7 @@ const Home = () => {
           <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
-
           <Bird />
-
           <Sky />
           <Island
             position={islandPosition}
